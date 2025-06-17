1124b040a681e1cd8122eab714161476fe1dbf9a
Richard <richard.yao@antithesis.com>
2025-06-17 17:50:57 -0400

    fix: docker-compose

--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -3,15 +3,15 @@
 echo "Cleaning up Geth and Lighthouse node data..."
 
 # Geth data dirs
-rm -rf ./geth/geth1/*
-rm -rf ./geth/geth2/*
-rm -rf ./geth/geth3/*
-rm -rf ./geth/geth4/*
+sudo rm -rf ./geth/geth1/*
+sudo rm -rf ./geth/geth2/*
+sudo rm -rf ./geth/geth3/*
+sudo rm -rf ./geth/geth4/*
 
 # Lighthouse node data dirs
-rm -rf ./lighthouse/cl_lighthouse1/*
-rm -rf ./lighthouse/cl_lighthouse2/*
-rm -rf ./lighthouse/cl_lighthouse3/*
-rm -rf ./lighthouse/cl_lighthouse4/*
+sudo rm -rf ./lighthouse/cl_lighthouse1/*
+sudo rm -rf ./lighthouse/cl_lighthouse2/*
+sudo rm -rf ./lighthouse/cl_lighthouse3/*
+sudo rm -rf ./lighthouse/cl_lighthouse4/*
 
 echo "Cleanup done."
